6fcd4ac6f16b1eb37323cd6bb74d85434207a98d
LudvigHN <ludvighaukrognielsen@gmail.com>
2026-09-11 11:32:02 +0200

    script generates image for each item for index page

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-
+const listDOM = document.querySelector(".list")
 
 fetch("data/destinations.json")
     .then(response => {return response.json()}
     )
     .then( data => {
         data.destinations.forEach(destination => {
-            console.log(destination);
+            const listItem = document.createElement("li")
+            listItem.classList.add("list__item")
+
+            const ItemImg = document.createElement("img")
+            ItemImg.classList.add("item__img")
+            ItemImg.setAttribute("src",`img/${destination.image}`)
+            
+            listItem.append(ItemImg)
+            listDOM.append(listItem)
+
             
         });
         
