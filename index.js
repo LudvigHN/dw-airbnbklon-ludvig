9b9b0f2cbe1329d8e14ac5d1bb40af462f23528c
LudvigHN <ludvighaukrognielsen@gmail.com>
2026-09-11 12:00:08 +0200

    div under images on homepage

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ fetch("data/destinations.json")
             const ItemImg = document.createElement("img")
             ItemImg.classList.add("item__img")
             ItemImg.setAttribute("src",`img/${destination.image}`)
-            
-            listItem.append(ItemImg)
+
+            const itemDiv = document.createElement("div")
+            itemDiv.classList.add("item__div")
+
+            const itemP = document.createElement("p")
+            itemP.classList.add("item_div__p")
+            itemDiv.append(itemP)
+            listItem.append(ItemImg,itemDiv)
             listDOM.append(listItem)
 
             
