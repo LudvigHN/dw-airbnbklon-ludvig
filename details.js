7cceada409237e3ef7c3e50101e02aae689d050a
Lunken N <LudvigHN>
2026-09-14 13:46:57 +0200

    added classes to details

--- details.js
+++ details.js
@@ -19,24 +19,30 @@ fetch(`data/${id}.json`)
 
         const detailsLocation = document.createElement("h2")
         detailsLocation.textContent = data.destination
+        detailsLocation.classList.add("details__location")
 
         const detailsTitle = document.createElement("h1")
         detailsTitle.textContent = data.title
+        detailsTitle.classList.add("details__title")
 
         const detailsSubTitle = document.createElement("h3")
         detailsSubTitle.textContent = data.subtitle
+        detailsSubTitle.classList.add("details__subtitle")
 
         const detailsText = document.createElement("p")
         detailsText.textContent = data.text
+        detailsText.classList.add("details__text")
 
         const facilitiesList = document.createElement("ul")
         facilitiesList.classList.add("facilities__list")
         
         const facilitiesHeader = document.createElement("h4")
+        facilitiesHeader.textContent = "Facilities"
         facilitiesList.append(facilitiesHeader)
 
         data.facilities.forEach(facility => {
             const facilitiesItem = document.createElement("li")
+            facilitiesItem.classList.add("details_list__item")
             facilitiesItem.textContent = facility
             facilitiesList.append(facilitiesItem)
         });
