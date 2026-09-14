13ef9a0f4615f6c4b5a355f6829a8fb2783d688d
Lunken N <LudvigHN>
2026-09-14 13:40:00 +0200

    js for details

---
+++ details.js
@@ -0,0 +1,51 @@
+const url = window.location.search
+
+const param = new URLSearchParams(url)
+
+const id = param.get("id")
+
+const main = document.querySelector("main")
+
+fetch(`data/${id}.json`)
+    .then(response => {return response.json()}
+    )
+    .then( data => {
+        const detailsImg = document.createElement("img")
+        detailsImg.setAttribute("src",`img/${data.image}`)
+        detailsImg.classList.add("details__img")
+
+        const detailsSection = document.createElement("section")
+        detailsSection.classList.add("details__section")
+
+        const detailsLocation = document.createElement("h2")
+        detailsLocation.textContent = data.destination
+
+        const detailsTitle = document.createElement("h1")
+        detailsTitle.textContent = data.title
+
+        const detailsSubTitle = document.createElement("h3")
+        detailsSubTitle.textContent = data.subtitle
+
+        const detailsText = document.createElement("p")
+        detailsText.textContent = data.text
+
+        const facilitiesList = document.createElement("ul")
+        facilitiesList.classList.add("facilities__list")
+        
+        const facilitiesHeader = document.createElement("h4")
+        facilitiesList.append(facilitiesHeader)
+
+        data.facilities.forEach(facility => {
+            const facilitiesItem = document.createElement("li")
+            facilitiesItem.textContent = facility
+            facilitiesList.append(facilitiesItem)
+        });
+
+        detailsSection.append(detailsLocation,detailsTitle,detailsSubTitle,detailsText,facilitiesList)
+        main.append(detailsImg,detailsSection)
+    }
+    
+        
+        
+    
+    )
